8ac2416c56994d44fd30f4db0becb095479cf134
Iver2101 <iverhaugen2000@gmail.com>
2022-09-16 11:47:00 -0400

    Tasklist sheit

--- js/comp.js
+++ js/comp.js
@@ -1,28 +1,33 @@
 export default class taskList extends HTMLElement {
     #shadow;
-    #callbacks = new Map();
-    #callbackID = 0;
+    //#callbacks = new Map();
+    //#callbackID = 0;
+#callbackStatus = null;
+
     #count = 0;
     constructor() {
         super();
         this.#shadow = this.attachShadow({ mode: 'closed' })
         this.#createHTML();
-const newtask = {
+        const newtask = {
     "id": 1,
     "title": "Do DAT152 home work",
     "status": "ACTIVE"
 };
-this.showTask(newtask);
-const status = {
-    "id": 1,
-    "status": "DONE"
+const newtask2 = {
+    "id": 2,
+    "title": "Do DAT152 home work",
+    "status": "ACTIVE"
 };
-this.updateTask(status);
+this.showTask(newtask2);
+this.showTask(newtask);
 
-this.setStatusesList();
-    this.changestatusCallback((id, newStatus) => {
+this.changestatusCallback(
+    (id, newStatus) => {
         console.log(`User chose ${newStatus} for task ${id}`)
     })
+    this.#shadow.querySelector("select").addEventListener("click",this.#aaa.bind(this))
+
 
     }
 
@@ -30,8 +35,8 @@ this.setStatusesList();
     showTask(task) {
 
         const taskObject = document.createElement('fieldset');
-        taskObject.id = "fieldset" + task.id;
-        const content = "<label for='task'>" + task.title + "</label><label for='status' id='status" +task.id+ "'>" + task.status + "</label>" + this.#makeButtons(task.id)
+        taskObject.id = `fieldset ${task.id}`;
+        const content = `<label for='task' id=label${task.id}>` + task.title + "</label><label for='status' id='status" +task.id+ "'>" + task.status + "</label>" + this.#makeButtons(task.id)
         taskObject.insertAdjacentHTML('beforeend', content);
         this.#shadow.getElementById('form').insertAdjacentElement('afterbegin', taskObject);
         this.#count++;
@@ -40,7 +45,7 @@ this.setStatusesList();
     }
 
     #makeButtons(id) {
-        return "<select name='modify' id='status" + id + "'></select> <input type='submit' value='remove' id='remove" + id + "'>"
+        return "<select name='modify' id='status" + id + "'></select> <button class='remove' id='remove" + id + "'>New Task</button>"
     }
 
     updateTask(status) {
@@ -60,30 +65,47 @@ this.setStatusesList();
         this.#shadow.getElementById('topText').innerHTML = "Found " + this.#count + " tasks."
     }
 
-    setStatusesList() {
+    setStatusesList(names) {
         const elems = this.#shadow.querySelectorAll("select");
-        elems.forEach(this.addOptions.bind(this))
+        elems.forEach(x => this.addOptions(x, names))
     }
     
     
-    addOptions(element) {
-        element.innerHTML =  "<option value='ACTIVE'>&lt;ACTIVE</option> <option value='DONE'>DONE</option> <option value='WAITING'>WAITING</option>"
+    addOptions(element, names) {
+        let out = "<option selected>&lt;modify&gt</option>";
+        names.forEach(x =>  out = out.concat(`<option value=${x}>${x}</option>`))
+        element.insertAdjacentHTML('beforeend',out);
 
     }
 
 
     enableAddTask() {
         this.#shadow.getElementById('addBtn').disabled = false;
-    }
+    }   
 
     addTaskCallback(func) {
         this.#shadow.getElementById('addBtn').addEventListener("click", func);
 
     }
 
+
+    #aaa(event) {
+        // finn nye status  legg status, og id i data
+        // Hvis callbackStatus ikke er null
+        if(this.#callbackStatus != null) {
+            const id = event.target.id.slice(-1)
+            const newStatus = event.target.value
+            if (window.confirm(`Set '${this.#shadow.getElementById('label' + id).innerHTML} to ${newStatus}`))
+                this.#callbackStatus(id, newStatus);
+        }
+    }
     changestatusCallback(func) {
-        const objs = this.#shadow.querySelectorAll("select")
-        objs.forEach(x => x.addEventListener('click', func))
+        this.#callbackStatus = func;
+    }
+
+    deleteTaskCallback(){
+        const objs = this.#shadow.querySelectorAll("button.remove");
+        //objs.forEach(x => x.addEventListener("click", ) )
     }
 
 
